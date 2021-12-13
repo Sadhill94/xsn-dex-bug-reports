fb439248d5c17f7edab2b893837a30aefeaae4d5
Sadhill94 <sadhill.dev@gmail.com>
2021-12-14 00:19:11 +0100

    New Create issue file type

---
+++ app/Models/IssueFile.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class IssueFile extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'src',
+        'issue_id',
+    ];
+
+    public function issues()
+    {
+        return $this->belongsTo(Issue::class);
+    }
+
+}
---
+++ database/migrations/2021_12_13_231329_create_issue_files_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIssueFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('issue_files', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('src');
+            $table->unsignedBigInteger('issue_id');
+            $table->foreign('issue_id')->references('id')->on('issues')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('issue_files');
+    }
+}
