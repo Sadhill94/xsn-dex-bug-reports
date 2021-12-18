d86533ffa17ad57cf54772da8a2f76f95c8d2a7b
Sadhill94 <sadhill.dev@gmail.com>
2021-12-18 14:35:51 +0100

    New Create factory for Issue and adapt seeds related

---
+++ database/factories/IssueFactory.php
@@ -0,0 +1,37 @@
+<?php
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Issue;
+use App\Models\Status;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class IssueFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Issue::class;
+    protected $osVersions = ['Windows', 'MacOs', 'Linux'];
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'description' => $this->faker->text(rand(25,70)),
+            'os' => $this->osVersions[rand(0, 2)],
+            'version' => '4.3.2',
+            'steps_to_reproduce' => $this->faker->text(rand(80,120)),
+            'extra_infos' => $this->faker->text(rand(30, 90)),
+            'status_id' => Status::inRandomOrder()->first(),
+            'category_id' => Category::inRandomOrder()->first()
+        ];
+    }
+}
+
--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,32 +4,39 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Ramsey\Uuid\Uuid;
 
 class CategorySeeder extends Seeder
 {
     public function run()
     {
         DB::table('categories')->insert([
+            'id' => Uuid::uuid4()->toString(),
             'name' => 'LND',
         ]);
 
         DB::table('categories')->insert([
+            'id' => Uuid::uuid4()->toString(),
             'name' => 'CONNEXT',
         ]);
 
         DB::table('categories')->insert([
+            'id' => Uuid::uuid4()->toString(),
             'name' => 'SWAPS',
         ]);
 
         DB::table('categories')->insert([
+            'id' => Uuid::uuid4()->toString(),
             'name' => 'MCLW',
         ]);
 
         DB::table('categories')->insert([
+            'id' => Uuid::uuid4()->toString(),
             'name' => 'UI',
         ]);
 
         DB::table('categories')->insert([
+            'id' => Uuid::uuid4()->toString(),
             'name' => 'SSUI',
         ]);
     }
