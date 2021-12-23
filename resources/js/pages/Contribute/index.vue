0b0825d5709309e4607637fa4bb93ce8d1a4ea86
Sadhill94 <sadhill.dev@gmail.com>
2021-12-23 23:09:44 +0100

    New Create the contribute page

--- resources/js/pages/Contribute/index.vue
+++ resources/js/pages/Contribute/index.vue
@@ -1,11 +1,16 @@
 <template>
-  <section class="min-h-screen max-h-screen flex justify-center items-center">
-    <h1 class="uppercase">Coming soon</h1>
-  </section>
+  <app-layout>
+    <section class="flex justify-center items-center absolute inset-0">
+      <h1 class="uppercase tracking-widest">Coming soon</h1>
+    </section>
+  </app-layout>
 </template>
 
 <script>
+import AppLayout from '@/layouts/AppLayout';
+
 export default {
   name: 'index',
+  components: { AppLayout },
 };
 </script>
